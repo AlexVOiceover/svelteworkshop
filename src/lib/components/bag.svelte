12c01278c620c612f66a91b0b1a5adac20a21ca2
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2025-03-13 12:46:34 +0000

    total and quantity displayes in the bag

--- src/lib/components/bag.svelte
+++ src/lib/components/bag.svelte
@@ -2,6 +2,10 @@
 	import type { Product } from '$lib/mockData'; // Adjust the path according to your project structure
 
 	export let items: Product[];
+
+	function totalCartAmount() {
+		return items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+	}
 </script>
 
 <div class="rounded bg-gray-100 p-4 shadow">
@@ -10,8 +14,14 @@
 		{#each items as item}
 			<li class="flex justify-between py-1">
 				<span>{item.name}</span>
+				<span>{item.quantity} x</span>
 				<span>{item.price} £</span>
 			</li>
 		{/each}
+
+		<li class="flex justify-between py-1">
+			<span>Total:</span>
+			<span>{totalCartAmount()}</span>
+		</li>
 	</ul>
 </div>
