629ff1eb0f955e2687895e4b27c6e3ab9d4eaab5
Alex Perez-Davies <=>
2025-03-13 15:43:06 +0000

    remove bag from homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,4 @@
 <div class="container mx-auto p-4">
 	<h1 class="mb-4 text-2xl font-bold">Home Page</h1>
 	<ProductsList products={products} onAddToBag={addToBag} />
-
-	<h2 class="mt-8 mb-4 text-xl font-bold">Your Bag</h2>
-	<Bag  />
 </div>
