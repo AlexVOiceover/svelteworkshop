494da49749d68e509ee72c3ca3c7b4c2f8220382
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2025-03-13 11:59:53 +0000

    bag component

---
+++ src/lib/components/bag.svelte
@@ -0,0 +1,14 @@
+<script module>
+	let total = 0;
+</script>
+
+<script>
+	total += 1;
+	console.log(`instantiated ${total} times`);
+</script>
+
+<!-- markup (zero or more items) goes here -->
+
+<style>
+	/* styles go here */
+</style>
